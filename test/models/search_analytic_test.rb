31abc34b4862c86aee0f1d6a4e9bf7095e4ac684
Hugo Melo <hugo@riseup.net>
2017-09-14 23:14:19 -0300

    Adjust test

--- test/models/search_analytic_test.rb
+++ test/models/search_analytic_test.rb
@@ -7,44 +7,62 @@ class SearchAnalyticTest < ActiveSupport::TestCase
   end
 
    test "it doesn't add new searches if user is still typing" do
-     key = "search:"
-     22.times {key += rand(0..9).to_s }
+     token = ""
+     22.times {token += rand(0..9).to_s }
      query = "How do I c"
      # user changes the query before check
-     $redis.set key, query + "ancel"
+     $redis.set "search:"+token, query + "ancel"
      # check is done with query changed
-     SearchAnalytic.check_new_search(key, query)
+     SearchAnalytic.check_new_search(token, query)
 
      assert_equal 0, SearchAnalytic.count
    end
 
    test "it doesn't add new searches if user is deleting text" do
-     key = "search:"
-     22.times {key += rand(0..9).to_s }
+     token = ""
+     22.times {token += rand(0..9).to_s }
      query = "How do I cancel?"
 
      # user has erased some chars before the job ran
-     $redis.set key, query[0..-4]
+     $redis.set "search:"+token, query[0..-4]
 
      # the job runs with older whole query
-     SearchAnalytic.check_new_search(key, query)
+     SearchAnalytic.check_new_search(token, query)
 
      assert_equal 0, SearchAnalytic.count
    end
 
    test "it does add new searches if user stopped typing" do
-     key = "search:"
-     22.times {key += rand(0..9).to_s }
+     token = ""
+     22.times {token += rand(0..9).to_s }
      query = "How do I c"
-     $redis.set key, query
+     $redis.set "search:"+token, query
 
-     SearchAnalytic.check_new_search(key, query)
+     SearchAnalytic.check_new_search(token, query)
 
      # right the same as before without this line:
      #$redis.set key, query + "ancel"
 
-     # here it should count
+     # it should count 1
      assert_equal 1, SearchAnalytic.count
+   end
+
+   test "it should group searches from the same user until it gets finished" do
+     token = ""
+     22.times {token += rand(0..9).to_s }
+
+     query = "How do I c"
+     $redis.set "search:"+token, query
+     SearchAnalytic.check_new_search(token, query)
+
+     query += "ancel my account"
+     $redis.set "search:"+token, query
+     SearchAnalytic.check_new_search(token, query)
+
+     # it should count only the last
+     assert_equal 1, SearchAnalytic.where("quantity > 0").count
+     assert_equal query, SearchAnalytic.last.key
+
    end
 
 end
